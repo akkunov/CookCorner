9510908cd6e33fb53f97c48709eadfec91d3481f
akkunov <akunov313131@gmail.com>
2024-07-21 19:45:16 +0600

    комииииит

--- src/components/ui/buttons/logout/logout.jsx
+++ src/components/ui/buttons/logout/logout.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import styles from './logout.module.css'
-import {useNavigate} from "react-router-dom";
-import {useActions} from "../../../../hook/useActions.jsx";
-function Logout(props) {
-    const navigate = useNavigate();
-    const {logout} = useActions()
-
-    function logoutHandler(){
-        logout();
-        navigate('/login')
-    }
-    return (
-        <button className={styles.logout} onClick={logoutHandler}>
-            Выйти
-        </button>
-    );
-}
-
-export default Logout;
+// import React from 'react';
+// import styles from './logout.module.css'
+// import {useNavigate} from "react-router-dom";
+// import {useActions} from "../../../../hook/useActions.jsx";
+// function Logout(props) {
+//     const navigate = useNavigate();
+//     const {logout} = useActions()
+//
+//     function logoutHandler(){
+//         logout();
+//         navigate('/login')
+//     }
+//     return (
+//         <button className={styles.logout} onClick={logoutHandler}>
+//             Выйти
+//         </button>
+//     );
+// }
+//
+// export default Logout;
