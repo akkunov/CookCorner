15a612e33046ccebe0c772d292e33755c223150c
akkunov <akunov313131@gmail.com>
2024-08-11 21:26:41 +0600

    mm

--- src/store/meal/mealService.ts
+++ src/store/meal/mealService.ts
@@ -1,11 +1,12 @@
 import {$auth} from "../../api/auth";
 import {AxiosResponse} from "axios";
 import {IMeal} from "./types";
+import {IAuthor} from "../user/types";
 
 export class MealService {
     static fetchMeals = async (): Promise<AxiosResponse<IMeal[]>> => $auth.get('/meals');
 
     static searchMeals = async (query: string): Promise<AxiosResponse<IMeal[]>> => $auth.get(`/meals/search?query=${query}`);
 
-    static searchByAuthor = async (author: string): Promise<AxiosResponse<IMeal[]>> => $auth.get(`/meals/author?name=${author}`);
+    static searchByAuthor = async (author: string): Promise<AxiosResponse<IAuthor>> => $auth.get(`/meals/author?name=${author}`);
 }
